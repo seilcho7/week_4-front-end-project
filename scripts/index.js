645871794757d0e98fcdc8aee380b0c47dc4a3f2
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-03-14 10:58:45 -0400

    makes fetch request from recipeAPI and stores json data locally

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const URL1 = ""
-
+const storageKey= "vegetarian-breakfast"
 // Adds function that fetch data from api
-function retrieveData() {
+function retrieveData1() {
     fetch(URL1)
         .then(function (response) {
             return response.json();
@@ -17,3 +17,59 @@ function getRelevantData(meal, dietary) {
 
     return URL1;
 }
+
+
+function accumulateRecipes(theActualData) {
+    recipeArray = [
+        ...Object.values(theActualData)
+    ];
+    storeRecipes(recipeArray);
+};
+
+function storeRecipes() {
+    const jsonRecipes = JSON.stringify
+    (recipeArray)
+    console.log(`saving ${recipeArray.length} recipes`);
+
+//  set that string in localStorage
+localStorage.setItem(storageKey,
+jsonRecipes);
+}
+function loadRecipes() {
+    //  get the JSON string from LocalStorage
+    const jsonRecipes = localStorage.getItem(storageKey);
+
+    //  convert it back into an array 
+    const arrayOfRecipes = JSON.parse(jsonRecipes);
+
+    console.log(`loaded ${arrayOfRecipes.length}recipes`)
+    // return it 
+    console.log(arrayOfRecipes)
+    return arrayOfRecipes
+
+}
+
+
+// Adds function that fetch data from api
+function retrieveData() {
+    fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=10&tags=vegetarian%2Cbreakfast", {
+  headers: {
+    "X-Rapidapi-Key": "8391aa4cdbmshde28ddc86fa9aecp1d4b08jsn757b24698283"
+  }
+})
+        .then(function (response) {
+            return response.json(); 
+        })
+        .then(accumulateRecipes)
+        .then(function (actualData) {
+            console.log(actualData);
+        })
+}
+
+function main () {
+    retrieveData() 
+    loadRecipes();
+    console.log(recipeArray);
+    
+}
+main(); 
